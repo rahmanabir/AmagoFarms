42fb219f1018a9f88855753f7e27322a624db518
rahmanabir <abir.rahman1995@gmail.com>
2019-06-30 12:25:56 +0600

    Update database.js

--- Amago_Server/config/database.js
+++ Amago_Server/config/database.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
 
+//Change connection details for local db
+//config files need to be hidden from git updates. Passwords need to be stored on a different file. 
 module.exports = new Sequelize('amago', 'postgres', '8953814', {
   host: 'localhost',
   dialect: 'postgres', 
