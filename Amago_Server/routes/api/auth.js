8eeaa6aa6faaab13bbb36508e60a6b036a504809
rahmanabir <abir.rahman1995@gmail.com>
2019-07-12 01:40:31 +0600

    Phone search api update

--- Amago_Server/routes/api/auth.js
+++ Amago_Server/routes/api/auth.js
@@ -7,4 +7,18 @@ const router = express.Router();
 const db = require('../../config/database');
 const Users = db.import('../../models/Users_updated');
 
+//const phone = req.body.phone;
+//const password = req.body.password;
+
+//GET single user
+router.post('/login', (req, res) =>
+  Users.findAll({
+    where: {
+      phone: req.body.phone
+    }
+  })
+    .then(result => res.json(result))
+    .catch(err => console.log(err))
+);
+
 module.exports = router;
--- Amago_Server/server.js
+++ Amago_Server/server.js
@@ -3,6 +3,7 @@ const server = express();
 
 //Routes
 const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
 
 //Logs server changes for debug
 const logger = require('./middleware/logger');
@@ -25,6 +26,7 @@ db.authenticate()
 
 //use routes
 server.use('/api/users', users);
+server.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000; // TODO Place this in config file
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
