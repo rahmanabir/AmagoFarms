ef67a334ae5d7eba2d07347c5407a94a58a5f28c
rahmanabir <abir.rahman1995@gmail.com>
2019-07-16 12:59:43 +0600

    JWT token verification and login complete

---
+++ Amago_Server/middleware/jwtMiddleware.js
@@ -0,0 +1,22 @@
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+// Verify Token
+module.exports = function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  //const bearerHeader = req.headers('authorization');
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+};
--- Amago_Server/routes/api/auth.js
+++ Amago_Server/routes/api/auth.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const uuid = require('uuid');
-const moment = require('moment');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -11,6 +9,9 @@ const Users = db.import('../../models/Users_updated');
 
 //Secret Key access for JWT signature validation
 const keys = require('../../config/keys');
+
+//JWT token extractor middleware
+const verifyToken = require('../../middleware/jwtMiddleware');
 
 //GET single user
 router.post('/loginTest', (req, res) =>
@@ -33,54 +34,57 @@ router.post('/login', (req, res) =>
     },
     raw: true //parsing seqeulize model
   })
-    .then(member =>
-      //member => res.status(404).json({ msg: 'member is not a registered user' })
-      //member => res.status(404).json(member)
-      //res.json(member)
-      {
-        //Check for user
-        if (!member) {
-          //console.log(member);
-          return res.status(404).json({ msg: ' not a registered user' });
-        }
-
-        const password = req.body.password; //password 
-        const memberData = member[0]; // member[0] is an array returned after query 
-        // Check Password
-
-        bcrypt.compare(password, memberData.password, (err, isMatch) => {
-          console.log(err + isMatch + password + memberData.password);
-          if (isMatch) {
-            // Passwords match
-            //Create JWT payload
-            console.log(memberData.password);
-            const payload = {
-              id: memberData.userID,
-              username: memberData.username
-            };
-
-            //Sign Token
-            jwt.sign(
-              payload,
-              keys.secretOrKey,
-              { expiresIn: 3600 },
-              (err, token) => {
-                res.json({
-                  token: token
-                  //success: true,
-                  //token: 'Bearer ' + token
-                });
-                //console.log(err);
-              }
-            );
-          } else {
-            // Passwords don't match
-            return res.status(400).json({ msg: 'Incorrect Password' });
-          }
-        });
+    .then(member => {
+      //Check for user
+      if (!member) {
+        return res.status(404).json({ msg: ' not a registered user' });
       }
-    )
+
+      const password = req.body.password; //password
+      const memberData = member[0]; // member[0] is an array returned after query
+      // Check Password
+
+      bcrypt.compare(password, memberData.password, (err, isMatch) => {
+        console.log(err + isMatch + password + memberData.password);
+        if (isMatch) {
+          // Passwords match
+          //Create JWT payload
+          console.log(memberData.password);
+          const payload = {
+            id: memberData.userID,
+            username: memberData.username
+          };
+
+          //Sign Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3060 },
+            (err, token) => {
+              res.json({
+                token: 'Bearer ' + token
+              });
+            }
+          );
+        } else {
+          // Passwords don't match
+          return res.status(400).json({ msg: 'Incorrect Password' });
+        }
+      });
+    })
     .catch(err => console.log(err))
 );
+
+router.post('/current', verifyToken, (req, res) => {
+  jwt.verify(req.token, keys.secretOrKey, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        authData
+      });
+    }
+  });
+});
 
 module.exports = router;
