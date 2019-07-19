28a8f8dae82a80bddd1ac3f3ce68db17b49705ff
rahmanabir <abir.rahman1995@gmail.com>
2019-07-19 22:20:24 +0600

    Harvest APIs added

---
+++ Amago_Server/routes/api/harvest.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const moment = require('moment');
+
+//User Model Import
+const db = require('../../config/database');
+const Harvest = db.import('../../models/Inventory');
+
+// @route GET api/harvest/allHarvest
+// @description Returns all harvests
+// @access Public
+router.get(
+  '/allHarvest',
+  (req, res) => Harvest.findAll().then(result => res.json(result)) // ! Can't handle large data load, needs to be fixed
+);
+
+router.get('/getHarvest/:id', (req, res) =>
+  Harvest.findAll({
+    where: {
+      userID: req.params.id
+    }
+  })
+    .then(harvest => res.json(harvest))
+    .catch(err => console.log(err))
+);
+
+router.post('/postHarvest', (req, res) => {
+  const newHarvset = {
+    userID: req.body.userID,
+    itemType: req.body.itemType,
+    amount: req.body.amount,
+    createdAt: moment().format(),
+    updatedAt: moment().format()
+  };
+
+  let { userID, itemType, amount, createdAt, updatedAt } = newHarvset;
+  // !Insert into Inventory Table
+  Harvest.create({
+    userID,
+    itemType,
+    amount,
+    createdAt,
+    updatedAt
+  })
+    .then(res.status(200).json({ msg: 'Harvest Added' }))
+    .catch(err => console.log(err));
+});
+
+module.exports = router;
